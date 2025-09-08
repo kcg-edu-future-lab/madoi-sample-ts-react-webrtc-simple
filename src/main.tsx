29c815445090f6cb4a9229fac40bf574764eb530
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2025-09-08 16:57:13 +0900

    roomIdも設定ファイルに記述する

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,10 @@ import './index.css'
 import { v4 as uuidv4 } from 'uuid';
 import App from './App.tsx'
 import { Madoi } from 'madoi-client';
-import { madoiUrl, madoiKey } from './keys.ts';
+import { madoiUrl, madoiKey, madoiRoomId } from './keys.ts';
 
-const roomId = "madoi-sample-ts-react-webrtc-simple-sadfo22023";
 const madoi = new Madoi(
-  `${madoiUrl}/${roomId}`, madoiKey,
+  `${madoiUrl}/${madoiRoomId}`, madoiKey,
   {id: uuidv4(), profile: {}});
 
 createRoot(document.getElementById('root')!).render(
